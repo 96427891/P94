f6067159bd2da4207970b41468a213d78d07ca12
96427891 <145680869+96427891@users.noreply.github.com>
2024-01-16 18:07:00 +0400

    Add files via upload

---
+++ kwitteroom.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDSEzW56rv1IKcwYXx_Ns3Xl_5DE6TQZr8",
+    authDomain: "lets-chat-web-app--project.firebaseapp.com",
+    projectId: "lets-chat-web-app--project",
+    storageBucket: "lets-chat-web-app--project.appspot.com",
+    messagingSenderId: "1017309638337",
+    appId: "1:1017309638337:web:03062dd71717da24bf2e40"
+  };
+  firebase.initializeApp(firebaseConfig);
